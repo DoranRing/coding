539170c103112c703fad558b6c18ac7cb7791964
doranring <doranring@qq.com>
2021-02-11 21:45:02 +0800

    add HardList,HardSorter

--- algorithms/sort.go
+++ algorithms/sort.go
@@ -1,5 +1,10 @@
 package algorithms
 
+import (
+	"log"
+	"os"
+)
+
 // BubbleSorter 冒泡排序(稳定排序)
 // time:O(n2), space:O(1)
 func BubbleSorter(nums []int) {
@@ -165,6 +170,187 @@ func BucketSorter(nums []int, max int) {
 			nums[idx] = i
 			idx++
 			bucket[i]--
+		}
+	}
+}
+
+// HardList 基于磁盘的线性表
+type HardList struct {
+
+	// 文件路径
+	FilePath string
+
+	// 线性表的长度
+	len int
+
+	// 打开的文件
+	f *os.File
+}
+
+func NewHardList(filePath string) *HardList {
+	list := &HardList{FilePath: filePath}
+	list.Open()
+	return list
+}
+
+func DefaultHardList() *HardList {
+	return NewHardList("hard_data")
+}
+
+// Open 打开磁盘文件
+func (h *HardList) Open() {
+	f, err := os.Create(h.FilePath)
+	if err != nil {
+		log.Fatalf("open file error: %s\n", err.Error())
+	}
+	h.f = f
+}
+
+// Close 关闭线性表
+func (h *HardList) Close() {
+	if h.f == nil {
+		return
+	}
+	err := h.f.Close()
+	if err != nil {
+		log.Fatalf("close file error: %s\n", err.Error())
+	}
+}
+
+// Len 线性表长度
+func (h *HardList) Len() int {
+	return h.len
+}
+
+// Get 获取指定索引的数据
+func (h *HardList) Get(idx int) int32 {
+	if idx >= h.len {
+		log.Fatalf("outbound index. len:%d,idx:%d\n", h.len, idx)
+	}
+	data := make([]byte, 4)
+	_, err := h.f.ReadAt(data, int64(idx*4))
+	if err != nil {
+		log.Fatalf("read file error: %s\n", err.Error())
+	}
+	return h.byteMapInt(data, 0)
+}
+
+// Set 设置指定索引的数据
+func (h *HardList) Set(idx int, num int32) {
+	if idx >= h.len {
+		log.Fatalf("outbound index, idx: %d, len: %d\n", idx, h.len)
+	}
+	data := h.intMapByte(num)
+	h.writeDate(data, int64(idx*4))
+}
+
+// Append 线性表尾部添加元素
+func (h *HardList) Append(num int32) {
+	data := h.intMapByte(num)
+	h.writeDate(data, int64(h.len*4))
+	h.len++
+}
+
+// AppendList 线性表尾部批量添加元素
+func (h *HardList) AppendList(nums []int32) {
+	data := make([]byte, 0, 4*len(nums))
+	for _, num := range nums {
+		data = append(
+			data,
+			byte(num>>24),     // 0-7 bit
+			byte(num<<8>>24),  // 8-15 bit
+			byte(num<<16>>24), // 16-23 bit
+			byte(num))         // 24-31bit
+	}
+
+	h.writeDate(data, int64(h.len*4))
+	h.len += len(nums)
+}
+
+// All 获取线性表的所有值
+func (h *HardList) All() []int32 {
+	data := make([]byte, 4*h.len)
+	_, err := h.f.ReadAt(data, 0)
+	if err != nil {
+		log.Fatalf("")
+	}
+	res := make([]int32, h.len, h.len)
+	for i := 0; i < len(data); i += 4 {
+		res[i/4] = h.byteMapInt(data, i)
+	}
+	return res
+}
+
+// writeDate 指定偏移量位置设置值
+func (h *HardList) writeDate(data []byte, offset int64) {
+	write, err := h.f.WriteAt(data, offset)
+	if err != nil {
+		log.Fatalf("write error: %s\n", err.Error())
+	}
+	if write != len(data) {
+		log.Fatalf("write error, data len: %d, write len: %d\n", len(data), write)
+	}
+}
+
+// intMapByte int转换成byte[]
+func (h *HardList) intMapByte(num int32) []byte {
+	data := make([]byte, 0)
+	data = append(
+		data,
+		byte(num>>24),     // 0-7 bit
+		byte(num<<8>>24),  // 8-15 bit
+		byte(num<<16>>24), // 16-23 bit
+		byte(num))         // 24-31bit
+
+	return data
+}
+
+// byteMapInt byte[]转换成int
+func (h *HardList) byteMapInt(nums []byte, idx int) int32 {
+	return int32(nums[idx])<<24 |
+		int32(nums[idx+1])<<16 |
+		int32(nums[idx+2])<<8 |
+		int32(nums[idx+3])
+}
+
+// MemorySorter 内存内排序
+func MemorySorter(nums []int32) {
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums)-i-1; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+			}
+		}
+	}
+}
+
+// HardSorter 磁盘内排序
+func HardSorter(nums []int32) {
+	hardList := DefaultHardList()
+	defer hardList.Close()
+	hardList.AppendList(nums)
+	hardSorter(hardList)
+}
+
+// HardSorterValid 测试磁盘内排序
+func HardSorterValid(nums []int32) []int32 {
+	hardList := DefaultHardList()
+	defer hardList.Close()
+	hardList.AppendList(nums)
+	hardSorter(hardList)
+	return hardList.All()
+}
+
+// hardSorter 磁盘内排序
+func hardSorter(hardList *HardList) {
+	for i := 0; i < hardList.len; i++ {
+		for j := 0; j < hardList.len-i-1; j++ {
+			if hardList.Get(j) > hardList.Get(j+1) {
+				cur := hardList.Get(j)
+				next := hardList.Get(j + 1)
+				hardList.Set(j, next)
+				hardList.Set(j+1, cur)
+			}
 		}
 	}
 }
--- algorithms/sort_test.go
+++ algorithms/sort_test.go
@@ -1,6 +1,7 @@
 package algorithms
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 	"time"
@@ -18,13 +19,13 @@ type sortTests struct {
 func getSortTests(len, max int) []sortTests {
 	return []sortTests{
 		//{"1", sortArgs{[]int{4, 2, 1, 3, 5, 0, 5}}},
-		{"2", sortArgs{genSortNums(len, max)}},
-		{"3", sortArgs{genSortNums(len, max)}},
-		{"4", sortArgs{genSortNums(len, max)}},
+		{"2", sortArgs{genIntNums(len, max)}},
+		{"3", sortArgs{genIntNums(len, max)}},
+		{"4", sortArgs{genIntNums(len, max)}},
 	}
 }
 
-func genSortNums(len, max int) []int {
+func genIntNums(len, max int) []int {
 	res := make([]int, len, len)
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < len; i++ {
@@ -34,7 +35,26 @@ func genSortNums(len, max int) []int {
 	return res
 }
 
+func genInt32Nums(len int, max int32) []int32 {
+	res := make([]int32, len, len)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < len; i++ {
+		val := rand.Int31n(max)
+		res[i] = val
+	}
+	return res
+}
+
 func sorted(nums []int) bool {
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] > nums[i+1] {
+			return false
+		}
+	}
+	return true
+}
+
+func sortedInt32(nums []int32) bool {
 	for i := 0; i < len(nums)-1; i++ {
 		if nums[i] > nums[i+1] {
 			return false
@@ -144,5 +164,58 @@ func BenchmarkBucketSorter(b *testing.B) {
 			},
 			1000, 500,
 		)
+	}
+}
+
+func TestHardList(t *testing.T) {
+	hardList := DefaultHardList()
+	defer hardList.Close()
+	var num1 int32 = 20
+	var num2 int32 = 21
+	hardList.Append(num1)
+	hardList.Append(num2)
+	val1 := hardList.Get(0)
+	val2 := hardList.Get(1)
+	if num1 != val1 {
+		t.Errorf("should: %d, actual: %d\n", num1, val1)
+	}
+	if num2 != val2 {
+		t.Errorf("should: %d, actual: %d\n", num2, val2)
+	}
+
+	var num3 int32 = 30
+	hardList.Set(1, num3)
+	val3 := hardList.Get(1)
+	if num3 != val3 {
+		t.Errorf("should: %d, actual: %d\n", num3, val3)
+	}
+
+	nums := hardList.All()
+	for _, num := range nums {
+		fmt.Printf("%d, ", num)
+	}
+}
+
+func TestHardSorter(t *testing.T) {
+	nums := genInt32Nums(1000, 1000*100)
+	sortedNums := HardSorterValid(nums)
+	if !sortedInt32(sortedNums) {
+		t.Errorf("not sorted")
+	}
+}
+
+// 844802 ns/op
+func BenchmarkMemorySorter(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		nums := genInt32Nums(1000, 1000*100)
+		MemorySorter(nums)
+	}
+}
+
+// 6438682600 ns/op
+func BenchmarkHardSorter(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		nums := genInt32Nums(1000, 1000*100)
+		HardSorter(nums)
 	}
 }
