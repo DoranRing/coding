b05f0eeb8ca657f6b587bfc31c77094838f77852
doranring <doranring@qq.com>
2021-02-15 19:17:23 +0800

    update 0033.search-in-rotated-sorted-array

--- leetcode/algorithms/0033.search-in-rotated-sorted-array.go
+++ leetcode/algorithms/0033.search-in-rotated-sorted-array.go
@@ -1,35 +1,40 @@
 package algorithms
 
-// time:O(1),space:O(1)
-//执行用时: 0 ms 内存消耗: 2.5 MB
-func searchNums(nums []int, target int) int {
-	l, r, idx := 0, len(nums)-1, -1
-	for l <= r {
-		mid := (l + r) / 2
-		if nums[mid] <= nums[len(nums)-1] {
-			idx = mid
-			r = r - 1
-		} else {
-			l = mid + 1
+// time:O(n),space:O(1)
+// 执行用时: 4 ms 内存消耗: 2.5 MB
+func searchInRotatedSortedArrayByLoop(nums []int, target int) int {
+	for idx, num := range nums {
+		if target == num {
+			return idx
 		}
 	}
-
-	res := searchNumsOperate(nums, 0, idx-1, target)
-	if res == -1 {
-		res = searchNumsOperate(nums, idx, len(nums)-1, target)
-	}
-	return res
+	return -1
 }
 
-func searchNumsOperate(nums []int, l, r, target int) int {
+// time:O(1),space:O(1)
+//执行用时: 4 ms 内存消耗: 2.5 MB
+func searchInRotatedSortedArrayByBinarySearch(nums []int, target int) int {
+	l, r := 0, len(nums)-1
 	for l <= r {
 		mid := (l + r) / 2
 		if nums[mid] == target {
 			return mid
-		} else if nums[mid] > target {
-			r = mid - 1
+		}
+
+		if nums[0] <= nums[mid] {
+			// 左边排序部分
+			if nums[0] <= target && target < nums[mid] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
 		} else {
-			l = mid + 1
+			// 右边排序部分
+			if nums[len(nums)-1] >= target && target > nums[mid] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
 		}
 	}
 	return -1
--- leetcode/algorithms/0033.search-in-rotated-sorted-array_test.go
+++ leetcode/algorithms/0033.search-in-rotated-sorted-array_test.go
@@ -15,11 +15,12 @@ func Test_searchNums(t *testing.T) {
 		{"1", args{nums: []int{4, 5, 6, 7, 0, 1, 2}, target: 0}, 4},
 		{"2", args{nums: []int{4, 5, 6, 7, 0, 1, 2}, target: 3}, -1},
 		{"3", args{nums: []int{-1}, target: 0}, -1},
+		{"4", args{nums: []int{4, 1}, target: 1}, 1},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := searchNums(tt.args.nums, tt.args.target); got != tt.want {
-				t.Errorf("searchNums() = %v, want %v", got, tt.want)
+			if got := searchInRotatedSortedArrayByBinarySearch(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("searchInRotatedSortedArrayByBinarySearch() = %v, want %v", got, tt.want)
 			}
 		})
 	}
