f978d4d89ded1433ca88eb8aca8ee6913221dfeb
doranring <doranring@qq.com>
2021-02-20 11:06:50 +0800

    update 0167.two-sum-ii-input-array-is-sorted

--- leetcode/algorithms/0167.two-sum-ii-input-array-is-sorted.go
+++ leetcode/algorithms/0167.two-sum-ii-input-array-is-sorted.go
@@ -1,27 +1,39 @@
 package algorithms
 
-// 执行用时: 4 ms 内存消耗: 2.9 MB
+// 利用序列数据特性，使用二分查找
 // time:O(n), space:O(1)
-func twoSum(numbers []int, target int) []int {
+// 执行用时: 4 ms 内存消耗: 2.9 MB
+func twoSumByBinarySearch(numbers []int, target int) []int {
 	for i := 0; i < len(numbers); i++ {
-		idx := twoSumByBinarySearch(numbers, target-numbers[i], i+1, len(numbers)-1)
-		if idx != -1 {
-			return []int{i + 1, idx + 1}
+		num, left, right := target-numbers[i], i+1, len(numbers)-1
+		for left <= right {
+			mid := (left + right) / 2
+			if numbers[mid] == num {
+				return []int{i + 1, mid + 1}
+			} else if numbers[mid] > num {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
 		}
 	}
 	return []int{}
 }
 
-func twoSumByBinarySearch(nums []int, num, left, right int) int {
-	for left <= right {
-		mid := (left + right) / 2
-		if nums[mid] == num {
-			return mid
-		} else if nums[mid] > num {
-			right = mid - 1
+// 通过碰撞指针
+// time:O(n), space:O(1)
+// 执行用时: 4 ms 内存消耗: 2.9 MB
+func twoSumByCollisionPointer(numbers []int, target int) []int {
+	l, r := 0, len(numbers)-1
+	for l < r {
+		sum := numbers[l] + numbers[r]
+		if sum == target {
+			return []int{l + 1, r + 1}
+		} else if sum < target {
+			l++
 		} else {
-			left = mid + 1
+			r--
 		}
 	}
-	return -1
+	return []int{}
 }
--- leetcode/algorithms/0167.two-sum-ii-input-array-is-sorted_test.go
+++ leetcode/algorithms/0167.two-sum-ii-input-array-is-sorted_test.go
@@ -22,8 +22,32 @@ func Test_twoSum(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := twoSum(tt.args.numbers, tt.args.target); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			if got := twoSumByBinarySearch(tt.args.numbers, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSumByBinarySearch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_twoSumByCollisionPointer(t *testing.T) {
+	type args struct {
+		numbers []int
+		target  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"1", args{[]int{2, 7, 11, 15}, 9}, []int{1, 2}},
+		{"2", args{[]int{2, 3, 4}, 6}, []int{1, 3}},
+		{"3", args{[]int{-1, 0}, -1}, []int{1, 2}},
+		{"4", args{[]int{5, 25, 75}, 100}, []int{2, 3}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSumByCollisionPointer(tt.args.numbers, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSumByCollisionPointer() = %v, want %v", got, tt.want)
 			}
 		})
 	}
